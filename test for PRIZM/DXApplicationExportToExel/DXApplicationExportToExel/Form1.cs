3190cc643f3043c8f4980bdac98ff1a7cc5bbcfa
DmitrySidorov <DmitrySidorov@mail.ua>
2014-09-25 15:39:12 +0300

    export to excel

---
+++ test for PRIZM/DXApplicationExportToExel/DXApplicationExportToExel/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace DXApplicationExportToExel
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+           // XLWorkbook wb = new XLWorkbook();
+            //DataTable dt = GetDataTableOrWhatever();
+           // wb.Worksheets.Add(dt, "WorksheetName");
+
+            //http://stackoverflow.com/questions/8207869/how-to-export-datatable-to-excel-in-c-sharp
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook ;
+            Excel.Worksheet xlWorkSheet ;
+
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[5, 5] = "some info";
+
+            xlWorkSheet.Cells[5, 6] = DateTime.Now.ToString();
+
+
+            string strName = DateTime.Now.ToString("dd MM yyyy hh-mm-ss") + "_csharp-Excel.xls";
+
+            xlWorkBook.SaveAs(strName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+            MessageBox.Show("Excel file created , you can find the file c:\\csharp-Excel.xls");
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
