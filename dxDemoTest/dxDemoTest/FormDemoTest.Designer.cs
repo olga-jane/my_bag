b99831839a094078b261dae44fdf71ade75e7014
DmitrySidorov <DmitrySidorov@mail.ua>
2014-09-27 00:44:13 +0300

    demo of Multilingual User Interface

---
+++ dxDemoTest/dxDemoTest/FormDemoTest.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace dxDemoTest
+{
+    partial class FormDxDemoTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxEditDemoTest = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.simpleButtonDemoTest = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEditDemoTest.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxEditDemoTest
+            // 
+            this.comboBoxEditDemoTest.Location = new System.Drawing.Point(13, 13);
+            this.comboBoxEditDemoTest.Name = "comboBoxEditDemoTest";
+            this.comboBoxEditDemoTest.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEditDemoTest.Size = new System.Drawing.Size(150, 20);
+            this.comboBoxEditDemoTest.TabIndex = 0;
+            this.comboBoxEditDemoTest.SelectedIndexChanged += new System.EventHandler(this.comboBoxEdit1_SelectedIndexChanged);
+            // 
+            // simpleButtonDemoTest
+            // 
+            this.simpleButtonDemoTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButtonDemoTest.Location = new System.Drawing.Point(218, 99);
+            this.simpleButtonDemoTest.Name = "simpleButtonDemoTest";
+            this.simpleButtonDemoTest.Size = new System.Drawing.Size(215, 23);
+            this.simpleButtonDemoTest.TabIndex = 1;
+            this.simpleButtonDemoTest.Text = "Do nothing";
+            // 
+            // FormDxDemoTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 134);
+            this.Controls.Add(this.simpleButtonDemoTest);
+            this.Controls.Add(this.comboBoxEditDemoTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormDxDemoTest";
+            this.Text = "Heder";
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEditDemoTest.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEditDemoTest;
+        private DevExpress.XtraEditors.SimpleButton simpleButtonDemoTest;
+
+    }
+}
+
---
+++ dxDemoTest/dxDemoTest/FormDemoTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// this references should be added
+using System.Globalization;
+using System.Resources;
+using System.Threading;
+using DevExpress.XtraEditors.Controls;
+
+// this atribute should be set culture as default
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+namespace dxDemoTest
+{
+    public partial class FormDxDemoTest : DevExpress.XtraEditors.XtraForm
+    {
+
+        ResourceManager rm;
+        ComboBoxItemCollection cultureNamesColl;
+
+        public FormDxDemoTest()
+        {
+            InitializeComponent();
+
+            comboBoxEditDemoTest.EditValue = "en-US";
+
+            cultureNamesColl = comboBoxEditDemoTest.Properties.Items;
+
+            cultureNamesColl.BeginUpdate();
+
+            try
+            {
+                cultureNamesColl.Add("en-US");
+                cultureNamesColl.Add("fr-FR");
+                cultureNamesColl.Add("ru-RU");
+                cultureNamesColl.Add("zh-CH");
+            }
+            finally
+            {
+                cultureNamesColl.EndUpdate();
+            }
+        }
+
+        private void comboBoxEdit1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxEditDemoTest.SelectedIndex >= 0 &&
+              comboBoxEditDemoTest.SelectedIndex < cultureNamesColl.Count)
+            {
+                // resource extraction
+                rm = new ResourceManager("dxDemoTest.Strings", typeof(Program).Assembly);
+
+                string cultureName = (string)cultureNamesColl[comboBoxEditDemoTest.SelectedIndex];
+
+                CultureInfo culture = CultureInfo.CreateSpecificCulture(cultureName);
+
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+
+                this.Text = rm.GetString("TimeHeader");
+
+                simpleButtonDemoTest.Text = rm.GetString("btnCapture");
+            }
+        }
+    }
+}
