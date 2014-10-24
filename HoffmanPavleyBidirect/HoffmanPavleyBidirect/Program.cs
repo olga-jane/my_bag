009f9f517a931c6c9860db257499edeaea2173c1
DmitrySidorov <DmitrySidorov@mail.ua>
2014-10-24 23:55:04 +0300

    HoffmanPavleyBidirect randering

--- HoffmanPavleyBidirect/HoffmanPavleyBidirect/Program.cs
+++ HoffmanPavleyBidirect/HoffmanPavleyBidirect/Program.cs
@@ -8,53 +8,55 @@ using QuickGraph;
 using QuickGraph.Algorithms;
 using QuickGraph.Algorithms.RankedShortestPath;
 
+using QuickGraph.Graphviz;
+
 namespace HoffmanPavleyBidirect
 {
     class Program
     {
+
+        public static int maxNumber = 100;
+
         static void Main(string[] args)
         {
-
+            Random rand = new Random();
             // the following code will give you th enumber 
             // and routes of all paths found in a Bidirectional 
             // graph using the  HoffmanPavley algorith.
 
-            BidirectionalGraph<string, TaggedEdge<string, string>> mvGraph2 = new BidirectionalGraph<string, TaggedEdge<string, string>>();
-            // UndirectedGraph<string, TaggedEdge<string, string>> mvGraph2 = new UndirectedGraph<string, TaggedEdge<string, string>>();
+            BidirectionalGraph<string, TaggedEdge<string, string>> graph = 
+                new BidirectionalGraph<string, TaggedEdge<string, string>>();
 
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("P1a", "I1", "P1a-I1"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I1", "P1a", "I1-P1a"));
+            for (int i = 0; i < maxNumber; ++i)
+            {
+                graph.AddVertex("V" + i.ToString());
+            }
 
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("P1a", "I3", "P1a-I3"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I3", "P1a", "I3-P1a"));
+            for (int i = 0; i < maxNumber; ++i)
+            {
+                int rn1 = rand.Next(maxNumber);
+                int rn2 = rand.Next(maxNumber);
 
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I1", "I2", "I1-I2"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I2", "I1", "I2-I1"));
-
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I1", "I3", "I1-I3"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I3", "I1", "I3-I1"));
-
-            //mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I3", "I2", "I3-I2"));
-            //mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I2", "I3", "I2-I3"));
-
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I3", "Leach", "I3-Leach"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("Leach", "I3", "Leach-I3"));
-
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("I2", "Leach", "I2-Leach"));
-            mvGraph2.AddVerticesAndEdge(new TaggedEdge<string, string>("Leach", "I2", "Leach-I2"));
+                graph.AddEdge(
+                    new TaggedEdge<string, string>("V" + rn1.ToString(),
+                                                   "V" + rn2.ToString(), "lable"));
+                graph.AddEdge(
+                    new TaggedEdge<string, string>("V" + rn2.ToString(),
+                                                   "V" + rn1.ToString(), "lable"));
+            }
             
             HoffmanPavleyRankedShortestPathAlgorithm<string, TaggedEdge<string, string>> test1 = 
-                new HoffmanPavleyRankedShortestPathAlgorithm<string, TaggedEdge<string, string>>(mvGraph2, E => 1.0);
+                new HoffmanPavleyRankedShortestPathAlgorithm<string, TaggedEdge<string, string>>(graph, E => 1.0);
 
-            test1.ShortestPathCount = 20;
+            test1.ShortestPathCount = 1000;
 
-            test1.SetRootVertex("P1a");
-            test1.SetRootVertex("Leach");
+            test1.SetRootVertex("V1");
+            test1.SetRootVertex("V" + maxNumber.ToString());
 
-            test1.Compute("P1a", "Leach");
+            test1.Compute("V1", "V" + (maxNumber - 1).ToString());
             
             
-            Console.WriteLine("Paths Found = " + test1.ComputedShortestPathCount.ToString());
+            
 
             foreach (IEnumerable<TaggedEdge<string, string>> path in test1.ComputedShortestPaths)
             {
@@ -66,9 +68,31 @@ namespace HoffmanPavleyBidirect
                 }
             }
 
+            Console.WriteLine("Paths Found = " + test1.ComputedShortestPathCount.ToString());
+
+
+
+            var graphViz = new GraphvizAlgorithm<string, TaggedEdge<string, string>>(graph, 
+                            @".\", QuickGraph.Graphviz.Dot.GraphvizImageType.Png);
+
+            graphViz.FormatVertex += FormatVertex;
+            //graphViz.FormatEdge += FormatEdge;
+
+            graphViz.Generate(new FileDotEngine(), "figure");
 
             Console.ReadKey();
 
+
+        }
+
+        private static void FormatVertex(object sender, FormatVertexEventArgs<string> e)
+        {
+            e.VertexFormatter.Label = e.Vertex;
+        }
+        private static void FormatEdge(object sender, FormatEdgeEventArgs<string, TaggedEdge<string, string>> e)
+        {
+            e.EdgeFormatter.Head.Label = e.Edge.Target;
+            e.EdgeFormatter.Tail.Label = e.Edge.Source;
         }
     }
 }
